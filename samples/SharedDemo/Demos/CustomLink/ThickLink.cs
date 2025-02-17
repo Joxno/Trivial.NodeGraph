9e58f4f34f0a48b2cc7e64743a3dd53a6c1259fe
Sebastian Georghiou <Joxno@users.noreply.github.com>
2025-02-17 00:59:21 +0000

    Removed branding. Removed CI/CD workflows, samples, docs, site.

--- samples/SharedDemo/Demos/CustomLink/ThickLink.cs
+++
@@ -1,8 +0,0 @@
-﻿using Blazor.Diagrams.Core.Models;
-
-namespace SharedDemo.Demos.CustomLink;
-
-public class ThickLink : LinkModel
-{
-    public ThickLink(PortModel sourcePort, PortModel targetPort = null) : base(sourcePort, targetPort) { }
-}
